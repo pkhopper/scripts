23297fb6ec558e2e5ad06cf2ba89bbb93bb8779b
pk <pk13610@gmail.com>
2014-07-15 13:27:45 +0800

    update

--- dlvideo/parsers/base_types.py
+++ dlvideo/parsers/base_types.py
@@ -4,8 +4,8 @@
 class VidParserBase:
     def info(self, url, vidfmt):
         """
-        return [urls], title, ext, multi-thread
-            multi-thread: bool, can download by multithread each url.
+        return [urls], title, ext, multi-thread, referer
+            multi-thread: bool, can download by multi-thread each url.
         """
         raise NotImplementedError(url)
 
--- dlvideo/parsers/iqiyi.py
+++ dlvideo/parsers/iqiyi.py
@@ -3,6 +3,7 @@
 import json
 from vavava import httputil
 from base_types import *
+from util import r1
 
 class Iqiyi():
     def __init__(self):
@@ -10,7 +11,7 @@ class Iqiyi():
 
     def real_url(self, url):
         import time
-        return json.loads(get_html(url[:-3]+'hml?v='+str(int(time.time()) + 1921658928)))['l'] # XXX: what is 1921658928?
+        return json.loads(self.http.get(url[:-3]+'hml?v='+str(int(time.time()) + 1921658928)))['l'] # XXX: what is 1921658928?
 
     def info(self, url, vidfmt):
         headers = {
@@ -22,7 +23,8 @@ class Iqiyi():
         videoId = r1(r'''data-player-videoid\s*[:=]\s*["']([^"']*)["']''', html)
         info_url = r'http://cache.video.qiyi.com/vms?key=fvip&src=p&vid={}'.format(videoId)
         html = self.http.get(info_url, headers=headers)
-        vstream = json.loads(html)['data']['vp']
+        js = json.loads(html)
+        vstream = js['data']['vp']
         # du = 'http://data.video.qiyi.com'
         # nvid = e2847b3b9b81469cbf7247ee8a1da61f
         du, dts, nvid = vstream['du'], vstream['dts'], vstream['nvid']
@@ -32,37 +34,38 @@ class Iqiyi():
                 real_url = 'http://data.video.qiyi.com/{}/videos{}'.format(nvid, l)
                 addr = html = self.http.get(real_url, headers=headers)
                 print addr
-        info_url = 'http://cache.video.qiyi.com/v/%s/%s/%s/' % (videoId, pid, ptype)
-        return info_url
-
-def iqiyi_download(url, merge=True):
-    # html = get_html(url)
-    # #title = r1(r'title\s*:\s*"([^"]+)"', html)
-    # #title = unescape_html(title).decode('utf-8')
-    # #videoId = r1(r'videoId\s*:\s*"([^"]+)"', html)
-    # #pid = r1(r'pid\s*:\s*"([^"]+)"', html)
-    # #ptype = r1(r'ptype\s*:\s*"([^"]+)"', html)
-    # #info_url = 'http://cache.video.qiyi.com/v/%s/%s/%s/' % (videoId, pid, ptype)
-    # videoId = r1(r'''data-player-videoid\s*[:=]\s*["']([^"']*)["']''', html, flag=re.I)
-    # assert videoId
-    # info_url = 'http://cache.video.qiyi.com/v/%s' % videoId
-
-    info_url = Iqiyi().get_info_url(url)
-    info_xml = get_html(info_url)
-
-    from xml.dom.minidom import parseString
-    doc = parseString(info_xml)
-    title = doc.getElementsByTagName('title')[0].firstChild.nodeValue
-    size = int(doc.getElementsByTagName('totalBytes')[0].firstChild.nodeValue)
-    urls = [n.firstChild.nodeValue for n in doc.getElementsByTagName('file')]
-    assert urls[0].endswith('.f4v'), urls[0]
-    urls = map(real_url, urls)
-    download_urls(urls, title, 'flv', total_size=size, merge=merge)
+        # info_url = 'http://cache.video.qiyi.com/v/%s/%s/%s/' % (videoId, pid, ptype)
+        # return info_url
+#
+# def iqiyi_download(url, merge=True):
+#     # html = get_html(url)
+#     # #title = r1(r'title\s*:\s*"([^"]+)"', html)
+#     # #title = unescape_html(title).decode('utf-8')
+#     # #videoId = r1(r'videoId\s*:\s*"([^"]+)"', html)
+#     # #pid = r1(r'pid\s*:\s*"([^"]+)"', html)
+#     # #ptype = r1(r'ptype\s*:\s*"([^"]+)"', html)
+#     # #info_url = 'http://cache.video.qiyi.com/v/%s/%s/%s/' % (videoId, pid, ptype)
+#     # videoId = r1(r'''data-player-videoid\s*[:=]\s*["']([^"']*)["']''', html, flag=re.I)
+#     # assert videoId
+#     # info_url = 'http://cache.video.qiyi.com/v/%s' % videoId
+#
+#     info_url = Iqiyi().get_info_url(url)
+#     info_xml = get_html(info_url)
+#
+#     from xml.dom.minidom import parseString
+#     doc = parseString(info_xml)
+#     title = doc.getElementsByTagName('title')[0].firstChild.nodeValue
+#     size = int(doc.getElementsByTagName('totalBytes')[0].firstChild.nodeValue)
+#     urls = [n.firstChild.nodeValue for n in doc.getElementsByTagName('file')]
+#     assert urls[0].endswith('.f4v'), urls[0]
+#     urls = map(real_url, urls)
+#     download_urls(urls, title, 'flv', total_size=size, merge=merge)
 
 def test():
     url = r'http://www.iqiyi.com/dianshiju/20120730/9682f22c54d70f29.html'
+    url = r'http://www.iqiyi.com/dianshiju/20120730/9682f22c54d70f29.html'
     qiyi = Iqiyi()
-    qiyi.get_info_url(url)
+    qiyi.info(url, 0)
 
 
 if __name__ == '__main__':
