6763765e05575b08f6f077c85948026e4737c478
pk <pk13610@gmail.com>
2014-08-03 07:44:12 +0800

    update

---
+++ dllive/config.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+from vavava import scriptutils
+
+class DLLiveConfig(scriptutils.BaseConfig):
+
+    def get_ini_attrs(self):
+        return {
+            'default  |outpath  |s': None,
+            'default  |channel  |s': None,
+            'default  |liveurl  |s': None,
+            'default  |addrfile |s': None,
+            'default  |npf      |i': None,
+            'network  |tmin     |i': None,
+            'network  |tmax     |i': None,
+            # 'proxy    |enable   |b': None,
+            # 'proxy    |addr     |s': None,
+            'favorites|         |s': lambda cfg: cfg.items('favorites'),
+            '         |log      | ': scriptutils.get_log_from_config(),
+            '         |proxyaddr| ': scriptutils.get_enabled_value_func('proxy', 'enable','addr'),
+        }
+
+
+
+    def get_args(self, argv):
+        usage = """./dllive [-i|l][-c config][-o out_put_path][-f favorite][-d duration]"""
+        import argparse
+        parser=argparse.ArgumentParser(usage=usage, version='0.1')
+        parser.add_argument('-c', '--config')
+        parser.add_argument('-u', '--liveurl')
+        parser.add_argument('-d', '--duration', type=float)
+        parser.add_argument('-o', '--outpath',)
+        parser.add_argument('-p', '--proxyaddr')
+        parser.add_argument('-f', '--favorite')
+        parser.add_argument('-i', '--interactive', action='store_true', default=False)
+        parser.add_argument('-l', '--channellist', action='store_true', default=False)
+        return parser.parse_args()
+
+
+if __name__ == "__main__":
+    import sys
+    cfg = DLLiveConfig()
+    cfg.read_cmdline_config('dllive.ini', argv=sys.argv)
+    print cfg
--- dllive/dllive.py
+++ dllive/dllive.py
@@ -23,59 +23,25 @@ pdirname = os.path.dirname
 pabspath = os.path.abspath
 
 
-class Config:
-    def __init__(self, config="dllive.ini"):
-        # script_dir = _util.script_path(__file__)
-        import ConfigParser
-        cfg = ConfigParser.ConfigParser()
-        if os.path.exists(config):
-            cfg.read(pabspath(config))
-        else:
-            cfg.read(pjoin(_util.script_path(__file__), config))
-        self.out_dir = cfg.get('default', 'out_dir')
-        self.channel = cfg.get('default', 'channel')
-        # self.duration = cfg.getint('default', 'duration')
-        self.log = cfg.get('default', 'log')
-        self.log_level = cfg.get('default', 'log_level')
-        self.live_url = cfg.get('default', 'live_url')
-        self.pythonpath = cfg.get('environment', 'pythonpath')
-        self.address_file = cfg.get('address_file', 'name')
-        self.proxy_addr = cfg.get('proxy', 'addr')
-        self.proxy_enable = cfg.getboolean('proxy', 'enable')
-        self.favorites = cfg.items('favorites')
-        lvlconvert = {
-            'critical': 50,
-            'fatal': 50,
-            'error': 40,
-            'warning': 30,
-            'warn': 30,
-            'info': 20,
-            'debug': 10,
-            'notset': 0
-        }
-        if self.log_level:
-            self.log_level = lvlconvert[self.log_level.strip().lower()]
-config = None
-log = None
-
-
 class M3u8Stream(ThreadBase):
-    TMP_PATH = '.dllive'
 
-    def __init__(self, log=None):
+    def __init__(self, tmin=10, tmax=20, proxy=None, log=None):
         ThreadBase.__init__(self, log=log)
-        self.__http = HttpUtil(timeout=10)
-        self.__old_urls = dict()
-        self.__urlworks_queue = Queue.Queue()
-        self.__axel = MiniAxelWorkShop(tmin=10, tmax=20, log=log)
-
-    def start_dl(self, url, fp, proxy=None):
+        self.__oldurls = dict()
+        self.__urlwks_q = Queue.Queue()
+        self.__axel = MiniAxelWorkShop(tmin=tmin, tmax=tmax, log=log)
+        self.__http = HttpUtil()
         if proxy:
-            self.__http.set_proxy({'http': proxy})
+            self.__http.set_proxy(proxy)
+
+    def start_dl(self, url, fp, npf):
+        """
+        start to download live stream spicified by url and write to fp
+        @param npf: download url stream by n parts per file
+        """
         self.index_url = url
         self.__ostream = fp
-        self.tmp_path = pjoin(config.out_dir, M3u8Stream.TMP_PATH)
-        _util.assure_path(self.tmp_path)
+        self.__npf = npf
         self.start()
 
     def run(self):
@@ -86,8 +52,8 @@ class M3u8Stream(ThreadBase):
             raise
         finally:
             self.log.debug('[M3u8Stream] stop')
-            if not self.__urlworks_queue.empty():
-                wk = self.__urlworks_queue.get()
+            if not self.__urlwks_q.empty():
+                wk = self.__urlwks_q.get()
                 wk.cleanUp()
             self.__axel.setToStop()
             self.__axel.join()
@@ -105,18 +71,18 @@ class M3u8Stream(ThreadBase):
                 last_index_at = start_at
                 urls, targetduration = self.__get_curr_index()
                 for url in urls:
-                    if url not in self.__old_urls:
+                    if url not in self.__oldurls:
                         memfile = BytesIO()
                         memfile.read = memfile.getvalue
-                        urlwork = DownloadUrl(url, out=memfile, n=3, log=self.log)
-                        self.__urlworks_queue.put(urlwork)
-                        self.__old_urls[url] = True
+                        urlwork = DownloadUrl(url, out=memfile, n=self.__npf, log=self.log)
+                        self.__urlwks_q.put(urlwork)
+                        self.__oldurls[url] = True
                         stream_len += targetduration
                         self.log.debug('[M3u8Stream] add a urlwork, %s', url)
 
-            if not curr_urlwk and not self.__urlworks_queue.empty():
-                curr_urlwk = self.__urlworks_queue.get()
-                self.__axel.addUrlWorks([curr_urlwk])
+            if not curr_urlwk and not self.__urlwks_q.empty():
+                curr_urlwk = self.__urlwks_q.get()
+                self.__axel.addUrlWork(curr_urlwk)
                 self.log.debug('[M3u8Stream] working on a new urlwork, %s', curr_urlwk.url)
 
             if not curr_urlwk:
@@ -202,44 +168,34 @@ class ClipDownloader(ThreadBase):
             raise
 
 
-class DownloadLiveStream(ThreadBase):
-
-    def __init__(self, log):
-        ThreadBase.__init__(self, log=log)
+class DownloadLiveStream:
+    def __init__(self, proxy=None, log=None):
         self.outfile = None
         self.duration = None
-        self.__m3u8 = M3u8Stream(log=log)
+        self.log = log
+        self.__m3u8 = M3u8Stream(proxy=proxy,log=log)
         self.__dl_handler = None
 
-    def __prepair(self, url, out_dir, duration, proxy):
+    def recode(self, url, duration=None, outpath='./', npf=3):
         assert duration is None or duration > 0
-        self.duration = duration
-        self.out_dir = out_dir
-        self.outfile = pjoin(out_dir, _util.get_time_string() + ".ts")
         self.url = url
-        self.proxy = proxy
-
-    def recode_in_this_thread(self, url, out_dir, duration=None, proxy=None):
-        self.__prepair(url, out_dir, duration, proxy)
-        self.__run_in_caller_thread = True
-        self.run()
-
-    def start_recode(self, url, out_dir, duration=None, proxy=None):
-        self.__prepair(url, out_dir, duration, proxy)
-        self.start()
-
-    def run(self):
+        self.duration = duration
+        self.outpath = outpath
+        _util.assure_path(self.outpath)
+        self.npf = npf
+        name = '%s.%s.ts' % (_util.get_time_string(), hash(url))
+        self.outfile = pjoin(outpath, name)
         self.log.info("|=> begin: %s", self.url)
         if self.duration:
-            self.log.info("|duration: %d", self.duration)
-        _util.assure_path(self.out_dir)
+            self.log.info("|=>duration: %d", self.duration)
         self.log.info("|=> output: %s", self.outfile)
+
         start_at = time.time()
         try:
             with open(self.outfile, 'wb') as fp:
                 self.__loop(fp=fp)
         except:
-            pass
+            raise
         finally:
             if self.__dl_handler:
                 self.__dl_handler.terminate_dl()
@@ -250,7 +206,7 @@ class DownloadLiveStream(ThreadBase):
 
     def __loop(self, fp):
         if self.url.find('m3u8') > 0 or DownloadLiveStream.__is_url_file(self.url):
-            self.__m3u8.start_dl(url=self.url, fp=fp, proxy=self.proxy)
+            self.__m3u8.start_dl(url=self.url, fp=fp, npf=self.npf)
         else:
             self.__dl_handler = ClipDownloader(self.url, fp)
             self.__dl_handler.start()
@@ -259,7 +215,7 @@ class DownloadLiveStream(ThreadBase):
         if self.duration:
             stop_at = start_at + self.duration
         # self.log.info('duration=%d, start=%f, stop=%f', self.duration, start_at, stop_at)
-        while not self.isSetToStop() or self.__run_in_caller_thread:
+        while True:
             loop_start_at = time.time()
             if self.duration and loop_start_at >= stop_at:
                 self.log.info("[DownloadLiveStream] time's up")
@@ -267,10 +223,6 @@ class DownloadLiveStream(ThreadBase):
             loop_duration = time.time() - loop_start_at
             if loop_duration < 1:
                 sleep(1)
-
-    def stopRecode(self):
-        if self.isAlive():
-            self.setToStop()
 
     @staticmethod
     def __is_url_file(url):
@@ -281,10 +233,9 @@ class DownloadLiveStream(ThreadBase):
         return info.type.find('url') > 0
 
 
-def interact(duration, out_dir, address_file, proxy=None):
-    log.info('===>interact mode')
+def interact(cfg):
     channel = raw_input('channel?')
-    with open(address_file, 'r') as fp:
+    with open(cfg.address_file, 'r') as fp:
         address = fp.readlines()
     sub_addr = dict()
     for addr in address:
@@ -298,84 +249,35 @@ def interact(duration, out_dir, address_file, proxy=None):
         channel_list.append(k)
         print '[%2d] %s  %s'%(index, k, v)
     channel_id = int(raw_input('id? ')) - 1
-    dls = DownloadLiveStream(log=log)
-    try:
-        dls.start_recode(url=sub_addr[channel_list[channel_id]],
-                         out_dir=out_dir, duration=duration, proxy=proxy)
-    except:
-        raise
-    finally:
-        dls.setToStop()
-        dls.join()
-
-def parse_args(config):
-    usage = """./dllive [-i|l][-c config][-o out_put_path][-f favorite][-d duration]"""
-    import argparse
-    parser=argparse.ArgumentParser(usage=usage, description='download live stream', version='0.1')
-    parser.add_argument('-c', '--config', default='dllive.ini')
-    parser.add_argument('-d', '--duration', type=float)
-    parser.add_argument('-o', '--out-path', dest='out_dir', default=config.out_dir)
-    parser.add_argument('-p', '--proxy', dest='proxy', action='store_const', const=True, help='http/https proxy')
-    parser.add_argument('-i', '--interactive', action='store_const', dest='interactive', const=True, help='interactive mode.')
-    parser.add_argument('-u', '--url', dest='url', help='live stream url')
-    parser.add_argument('-f', '--favorite', dest='favorite', help='favorite channel name, define in config file.')
-    parser.add_argument('-l', '--channel-list', dest='channellist', action='store_const', const=True, help='update addresses.')
-    args = parser.parse_args()
-    print args
-    return args
-
-
-def init_args_config():
-    config = Config()
-    args = parse_args(config)
-    if args.config != 'dllive.ini':
-        config = Config(config=args.config)
-        args = parse_args(config)
-    log = _util.get_logger(logfile=config.log, level=config.log_level)
-    return args, config, log
+    return sub_addr[channel_list[channel_id]]
 
 
 def main():
-    global log
-    global config
-    args, config, log = init_args_config()
-    log.info('{}'.format(args))
-    live_url = config.live_url
-    proxy = None
-    if args.proxy:
-        proxy = config.proxy_addr
-    if args.interactive:
-        interact(duration=args.duration, out_dir=args.out_dir,
-                 address_file=config.address_file, proxy=proxy)
-        return
-    elif args.channellist:
-        script_path = _util.script_path(__file__)
-        os.system('python %s/xbmc_5ivdo.py -t 直播 > %s'%(
-            script_path, config.address_file))
-        return
-    elif args.favorite:
-        for favorite in config.favorites:
-            if favorite[0] == args.favorite:
-                live_url = favorite[1]
-                break
-    elif args.url:
-        live_url = args.url
-
-    dls = DownloadLiveStream(log=log)
+    from config import DLLiveConfig
+    from sys import argv
+    cfg = DLLiveConfig().read_cmdline_config('dllive.ini', __file__, argv)
+    log = cfg.log
+    dls = DownloadLiveStream(proxy=cfg.proxyaddr, log=log)
+    liveurl = cfg.liveurl
     try:
-        dls.recode_in_this_thread(url=live_url, out_dir=args.out_dir,
-                         duration=args.duration, proxy=proxy)
-    except Exception as e:
-        log.exception(e)
-        raise
-    # finally:
-    #     if not dls.isAlive():
-    #         dls.stopRecode()
-    #         dls.join()
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt as e:
+        if cfg.interactive:
+            liveurl = interact(cfg)
+        elif cfg.favorite:
+            for favorite in cfg.favorites:
+                if favorite[0] == cfg.favorite:
+                    liveurl = favorite[1]
+                    break
+        elif cfg.channellist:
+            script_path = _util.script_path(__file__)
+            os.system('python %s/xbmc_5ivdo.py -t 直播 > %s'%(
+                script_path, cfg.address_file))
+            return
+        dls.recode(url=liveurl, duration=cfg.duration, outpath=cfg.outpath, npf=cfg.npf)
+    except KeyboardInterrupt:
         print 'stop by user'
         exit(0)
+    except Exception as e:
+        log.exception(e)
+
+if __name__ == "__main__":
+    main()
