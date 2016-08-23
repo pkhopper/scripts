546ee9c607db589aab2e201ab1bfe1869834fbb9
pkhopper <letian.llt@live.com>
2016-08-23 17:40:41 +0800

    add deploy script

--- backup/deploy.sh
+++ backup/deploy.sh
@@ -27,6 +27,10 @@ make install
 cd ..
 
 
+PATH=$HOME/local/bin:$PATH
+EXPORT $PATH
+
+
 echo "download setuptools-26.0.0.tar.gz"
 if [ ! -f "setuptools-26.0.0.tar.gz" ]; then
 	curl "https://pypi.python.org/packages/0d/13/ce6a0a22220f3da7483131bb8212d5791a03c8c3e86ff61b2c6a2de547cd/setuptools-26.0.0.tar.gz#md5=846e21fea62b9a70dfc845d70c400b7e" -o setuptools-26.0.0.tar.gz
@@ -47,7 +51,7 @@ python setup.py install
 cd ..
 
 
-echo "download django.1.9.4.tar.gz"
+echo "download supervisor"
 if [ ! -d "supervisor" ]; then
 	git clone "https://github.com/Supervisor/supervisor.git"
 fi
