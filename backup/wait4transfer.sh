7870ba8f06d35e58818394018a5f82cd92d426e3
pkhopper <letian.llt@live.com>
2016-08-23 16:57:23 +0800

    xxxx

--- backup/wait4transfer.sh
+++ backup/wait4transfer.sh
@@ -2,13 +2,15 @@ TMP="./xxxtransferdata.zip"
 URL=https://raw.githubusercontent.com/pkhopper/vavava/master/vavava/transfer.py
 
 if [ ! -f "./transfer.py" ]; then
-  wget $URL
+  curl $URL -o "./transfer.py"
 fi
 
 if [! -f "$TMP" ]; then
   rm $TMP
 fi
 
-python transfer.py --force --server --ip 0.0.0.0 --port 1237 --outputfile $TMP
+chmod +x "./transfer.py"
+
+python transfer.py --force --server --ip 0.0.0.0 --port 1239 --outputfile $TMP
 
 unzip $TMP
